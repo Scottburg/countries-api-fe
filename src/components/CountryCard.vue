87256bf56b6358694301d36551627ae7b67d9648
S B <scottb4545@gmail.com>
2023-01-27 15:35:54 +0000

    set up in vue and first page logic added

---
+++ src/components/CountryCard.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  props: { country: { type: Object, required: true } },
+  created() {
+    // console.log(this.country);
+  },
+
+  // created() {
+  //   console.log(this.props);
+  // },
+};
+
+// const countryDetails = {
+//   population: props.country.population.toLocaleString(),
+//   region: props.country.region,
+//   capital: props.country.capital?.[0],
+// };
+// const showDetails = () => {
+//   const currentCountry = props.country.name.common.toLowerCase();
+//   router.push({ path: `/${currentCountry}` });
+// };
+</script>
+
+<template>
+  <section class="country-card">
+    <!-- COUNTRY CARD {{ country }} -->
+    <img
+      :src="country.flags.png"
+      :alt="`${country.name.common} flag`"
+      @click="toggleModal"
+    />
+    <h4>{{ country.name.common }}</h4>
+    <h4>Population:{{ country.population }}</h4>
+    <p>Region: {{ country.region }}</p>
+    <p>Capital: {{ country.capital ? country.capital[0] : 'None' }}</p>
+  </section>
+</template>
+
+<style>
+.country-card {
+  border: 1px black;
+}
+</style>
---
+++ src/components/CountrySearchHome.vue
@@ -0,0 +1,117 @@
+<script>
+import CountryCard from './CountryCard.vue';
+import axios from 'axios';
+export default {
+  components: {
+    CountryCard,
+  },
+  data() {
+    return {
+      countries: null,
+      selectedCountries: [],
+      inputValue: '',
+      loading: true,
+      regions: [
+        'All',
+        'Africa',
+        'Americas',
+        'Asia',
+        'Europe',
+        'Oceania',
+        'Antarctic',
+      ],
+      selectedRegion: 'All',
+    };
+  },
+  created() {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then((response) => {
+        this.countries = response.data;
+        this.loading = false;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+  methods: {
+    async getCountries() {
+      try {
+        const response = await axios.get('https://restcountries.com/v3.1/all');
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    filterCountries() {
+      let filter = this.inputValue;
+
+      if (this.selectedRegion && this.selectedRegion !== 'All') {
+        let filteredSelection = this.countries.filter((country) => {
+          return (
+            country.region.toLowerCase() === this.selectedRegion.toLowerCase()
+          );
+        });
+        if (filter !== '') {
+          filteredSelection = filteredSelection.filter((country) =>
+            country.name.common.toLowerCase().includes(filter)
+          );
+        }
+        this.selectedCountries = filteredSelection;
+      } else {
+        this.selectedCountries = this.countries.filter((country) =>
+          country.name.common.toLowerCase().includes(filter)
+        );
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+    <div>HEADER HERE WITH DARK MODE TOGGLE - FLEX</div>
+    <article>
+      <input
+        type="text"
+        placeholder="Search for a country..."
+        v-model="inputValue"
+        v-on:input="filterCountries"
+      />
+      <button @click="filterCountries">Search</button>
+      <div>DROP DOWN SELECTOR FOR REGION</div>
+      <select
+        name="filter-by-region"
+        id="filter-by-region"
+        v-model="selectedRegion"
+        @change="filterCountries"
+      >
+        <option value="">Filter by region</option>
+        <option v-for="(region, index) in regions" :key="index" :value="region">
+          {{ region }}
+        </option>
+      </select>
+    </article>
+    <article>
+      <div>GRID OF COUNTRIES CARDS</div>
+
+      <div v-if="selectedCountries.length > 0" class="container">
+        <div v-for="country in selectedCountries">
+          <CountryCard :country="country" />
+        </div>
+      </div>
+      <div
+        v-else-if="(countries && selectedCountries === null) || !loading"
+        class="container"
+      >
+        <div v-for="country in countries">
+          <CountryCard :country="country" />
+        </div>
+      </div>
+      <!-- <div v-if="selectedCountries.length === 0">No Countries Found</div> -->
+      <div v-else-if="loading">Loading ....</div>
+    </article>
+  </main>
+</template>
+
+<style scoped></style>
